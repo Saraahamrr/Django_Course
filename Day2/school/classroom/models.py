ae7da96ff0c47f7ab69e3dadaae042dda1040ea0
saraahamrr <saraahamrr98@gmail.com>
2025-03-04 00:06:54 +0200

    task1&2v.1

--- Day2/school/classroom/models.py
+++ Day2/school/classroom/models.py
@@ -7,6 +7,12 @@ class Classroom(models.Model):
     year = models.IntegerField()
     teacher = models.CharField(max_length=30)
     student = models.ForeignKey('Student', on_delete=models.CASCADE,related_name='classrooms')
+    School = models.ForeignKey('School', on_delete=models.CASCADE,related_name='classrooms_associated' ,default=1, null=True, blank=True)
+    area = models.FloatField()
+    
+    def save (self,*args,**kwargs):
+        self.area = round(self.area,2)
+        super(Classroom,self).save(*args,**kwargs)
     def __str__(self):
         return self.name
 
@@ -21,10 +27,12 @@ class Subject(models.Model):
     
 class School(models.Model):
     name = models.CharField(max_length=30)
-    number_of_classrooms = models.IntegerField(models.Count('classrooms'))
     address = models.TextField()
-    classrooms = models.OneToOneField(Classroom,on_delete=models.CASCADE,related_name='schools')
-    
+    classrooms = models.ForeignKey(Classroom, on_delete=models.CASCADE, related_name='schools')
+    area = models.FloatField()
+    def save (self,*args,**kwargs):
+        self.area = round(self.area,2)
+        super(School,self).save(*args,**kwargs)
     
     def __str__(self):
         return self.name
--- Day2/school/classroom/views.py
+++ Day2/school/classroom/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from .models import Classroom
+from .models import Subject
+from .models import School
+from .models import Student
 
 # Create your views here.
 def home(request):
-    return render(request, 'myClassroom/index.html', {})
+    return render(request, 'myClassroom/index.html', {
+        'classrooms': Classroom.objects.all(),
+        'subjects': Subject.objects.all(),
+        'schools': School.objects.all(),
+        'students': Student.objects.all
+    })
 
 def about(request): 
     return render(request, 'myClassroom/about.html', {})
